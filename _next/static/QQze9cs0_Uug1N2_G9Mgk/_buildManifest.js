3a6588a15ef810da897897914b8ca2a22071ddb6
Franny <derek@bloguin.com>
2026-06-09 19:43:53 -0400

    Updates

--- _next/static/QQze9cs0_Uug1N2_G9Mgk/_buildManifest.js
+++ _next/static/QQze9cs0_Uug1N2_G9Mgk/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/knightline//_next/:path+",
+        "destination": "/knightline/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
